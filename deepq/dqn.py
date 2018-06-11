8c9ea9b265fca318f1f6b69b5f8c154887552d25
Chaskin Saroff <chaskin.saroff@gmail.com>
2018-06-10 18:29:33 -0700

    Checkpoint before profiling

--- deepq/dqn.py
+++ deepq/dqn.py
@@ -3,11 +3,6 @@ import gym
 import numpy as np
 import tensorflow as tf
 
-# Imports specifically so we can render outputs in Jupyter.
-from JSAnimation.IPython_display import display_animation
-from matplotlib import animation
-from IPython.display import display
-
 import keras
 from keras.models import Sequential, load_model
 from keras.layers import Dense, Dropout, LSTM
@@ -16,6 +11,7 @@ from keras.callbacks import TensorBoard
 from baselines.deepq.replay_buffer import ReplayBuffer
 
 from math import inf
+from timeit import default_timer as timer
 
 import random
 
@@ -36,40 +32,59 @@ import random
 # obvious difficulty extending this approach to any infinite action space, but
 # we will refrain from addressing this here.
 def simple_nn(obs_shape, num_actions):
+    print('Creating model with observation shape', obs_shape)
+    print('Creating model with ', num_actions, 'actions')
     model = Sequential()
     print('input_shape', obs_shape)
-    model.add(Dense(64, activation='relu', input_shape=obs_shape))
-    model.add(Dense(64, activation='relu'))
+    model.add(Dense(8, activation='relu', input_shape=obs_shape))
+    model.add(Dense(8, activation='relu'))
     model.add(Dense(num_actions, activation='linear'))
     model.compile(loss=tf.losses.huber_loss, optimizer='adam', metrics=['accuracy'])
     return model
 
 # fit target_model using model as the predictor for future rewards
-def fit_batch(model, target_model, num_acts, gamma, batch):
+def fit_batch(model, target_model, num_acts, gamma, batch, tensorboard):
+    # with tf.Session() as sess:
     (obs, acts, rewards, new_obs, is_dones) = batch
     # Predict the value of the q function in the next state for each action
     # And take the outcome for the best action
-    q_tp1_bests = tf.reduce_max(model.predict(new_obs), 1)
+    predict_q_tp1_time = timer()
+    q_tp1 = model.predict(new_obs)
+    print('predict_q_tp1_time', timer() - predict_q_tp1_time)
+
+    q_tp1_best_time = timer()
+    q_tp1_bests = tf.reduce_max(q_tp1, 1)
+    print('predict best q_tp1 time', timer() - q_tp1_best_time)
 
     # If is_done, there is no expected future reward because our current episode
     # is finished.  This should "ground" the model since this is the only fully
     # correct(non-estimated) q value.
+    eoe_reward_time = timer()
     q_tp1_bests = (1.0 - is_dones) * q_tp1_bests
+    print('end of episode reward time', timer() - eoe_reward_time)
 
     # Add actual current reward to expected future reward to generate the
     # targets for the training examples
+    agg_reward_time = timer()
     q_t_bests = rewards + gamma * q_tp1_bests
+    print('aggregate reward time', timer() - agg_reward_time)
 
     # Compute an additional forward pass to estimate the q values for the
     # actions that were not taken in order to avoid computing the error manually
+    pred_q_t_time = timer()
     q_targets = model.predict(obs)
+    print('predict q(t)', timer() - pred_q_t_time)
 
     # Replace predicted reward with observed reward
+    q_t_replace_time = timer()
     q_targets = q_targets * tf.one_hot(acts, num_acts, on_value=0.0, off_value=1.0)
     q_targets = q_targets + tf.reshape(q_t_bests, [-1, 1]) * tf.one_hot(acts, num_acts, on_value=1.0, off_value=0.0)
+    print('Replace pred reward with obs reward time', timer() - q_t_replace_time)
 
     # Fit the model using training examples
-    model.fit(obs, q_targets, epochs=1, steps_per_epoch=1)
+    fit_model_time = timer()
+    model.fit(obs, q_targets, epochs=1, steps_per_epoch=1, verbose=True, callbacks=[tensorboard])
+    print('Model fit time', timer() - fit_model_time)
 
 def clone_model(model):
     clone = keras.models.clone_model(model)
@@ -86,94 +101,108 @@ def clone_model(model):
 # decorelate our training examples.
 # 3. Sample from the replay buffer to improve our new policy.
 # 4. Periodically rotate the new policy in as the current policy
-def learn(env,
+def learn(envname,
           learning_rate=5e-4,
           max_timesteps=100000,
           buffer_size=100000,
-          epsilon_decay=0.9999,
-          train_freq=10,
+          epsilon_decay=0.999,
+          train_freq=1,
           batch_size=32,
-          print_freq=100,
-          checkpoint_freq=10000,
+          print_freq=10,
+          checkpoint_freq=100,
+          learning_starts=1000,
           gamma=0.99,
           target_network_update_freq=500,
-          episode_render_freq=100):
+          episode_render_freq=None,
+          log_dir='./tensorboard'):
 
-    epsilon = lambda t: epsilon_decay ** t
+        tensorboard = TensorBoard(log_dir=log_dir + '/' + envname)
 
-    replay_buffer = ReplayBuffer(buffer_size)
-    num_actions = env.action_space.n
+        # Create a breakout environment
+        env = gym.make(envname)
 
-    # Here, we'll use a simple feed forward nn for representing
-    # Q(s) -> [r_1, r_2, ..., r_n] where r_k is the reward for taking action
-    # `k` in state `s`
-    model = simple_nn(env.observation_space.shape, num_actions)
-    target_model = clone_model(model)
+        epsilon = lambda t: epsilon_decay ** t
 
-    # The current timestep, t
-    t = 0
+        replay_buffer = ReplayBuffer(buffer_size)
+        num_actions = env.action_space.n
 
-    # Keep some state about the current episode
-    num_episodes = 0
-    episode_total_reward = 0
-    episode_timesteps = 0
-    num_epiosodes = 0
-    last_checkpoint_mean_reward = -inf
-    last_episode_mean_reward = -inf
+        # Here, we'll use a simple feed forward nn for representing
+        # Q(s) -> [r_1, r_2, ..., r_n] where r_k is the reward for taking action
+        # `k` in state `s`
+        model = simple_nn(env.observation_space.shape, num_actions)
+        target_model = clone_model(model)
 
-    # Start off with a fresh environment
-    obs = env.reset()
+        # The current timestep, t
+        # t = 0
 
-    # Play breakout for max_timesteps
-    for i in range(max_timesteps):
-        # With probability epsilon, take a random action
-        if(random.uniform(0, 1) < epsilon(t)):
-            action = env.action_space.sample()
-        else:
-            action = tf.argmax(model.predict(obs), 1)
+        # Keep some state about the current episode
+        num_episodes = 0
+        episode_total_reward = 0
+        episode_timesteps = 0
+        num_epiosodes = 0
+        last_checkpoint_mean_reward = -inf
+        last_episode_mean_reward = -inf
 
-        # Collect observations and store them for replay
-        new_obs, reward, is_done, _ = env.step(action)
-        replay_buffer.add(obs, action, reward, new_obs, is_done)
-        obs = new_obs
+        # Start off with a fresh environment
+        obs = env.reset()
 
-        # Update logging info
-        episode_total_reward += reward
-        episode_timesteps += 1
+        # Play breakout for max_timesteps
+        for t in range(max_timesteps):
+            # With probability epsilon, take a random action
+            if(random.uniform(0, 1) < epsilon(t)):
+                action = env.action_space.sample()
+            else:
+                observations = np.reshape(obs, [1, -1])
+                q_values = model.predict(observations)
+                action = np.argmax(q_values, axis=1)[0]
 
-        if t % train_freq == 0:
-            fit_batch(model, target_model, num_actions, gamma, replay_buffer.sample(batch_size))
+            # Collect observations and store them for replay
+            new_obs, reward, is_done, _ = env.step(action)
+            replay_buffer.add(obs, action, reward, new_obs, is_done)
+            obs = new_obs
 
-        if t % target_network_update_freq == 0:
-            model = target_model
-            target_model = clone_model(model)
+            # Update logging info
+            episode_total_reward += reward
+            episode_timesteps += 1
 
-        if episode_timesteps % print_freq == 0:
-            print("timesteps", t)
-            print("episodes run", num_episodes)
-            print("mean reward", (episode_total_reward / episode_timesteps))
+            if t > learning_starts and t % train_freq == 0:
+                start = timer()
+                fit_batch(model, target_model, num_actions, gamma, replay_buffer.sample(batch_size), tensorboard)
+                print('Training for timestep ', t, ' took', timer() - start)
+                print('End timestep ', t, '-------------------------------')
 
-        if t % checkpoint_freq == 0 and last_episode_mean_reward > last_checkpoint_mean_reward:
-            print("Saving model due to mean reward increase: ", last_checkpoint_mean_reward, " -> ", last_episode_mean_reward)
-            model.save('models/' + ctx.envname + '_deepq.h5py')
-            last_checkpoint_mean_reward = last_episode_mean_reward
+            if t > learning_starts and t % target_network_update_freq == 0:
+                model = target_model
+                target_model = clone_model(model)
+                print('Setting model to target model')
 
-        if is_done:
-            obs = env.reset()
-            last_episode_mean_reward = episode_total_reward / episode_timesteps
-            episode_total_reward = 0
-            episode_timesteps = 0
-            num_episodes += 1
+            if is_done and num_episodes % print_freq == 0:
+                print("timesteps", t)
+                print("episodes run", num_episodes)
+                print("last episode reward", episode_total_reward)
+                print("% time spent exploring", int(100 * epsilon(t)))
 
-        if num_episodes % episode_render_freq == 0:
-            env.render()
+            if t % checkpoint_freq == 0 and last_episode_mean_reward > last_checkpoint_mean_reward:
+                print("Saving model due to mean reward increase: ", last_checkpoint_mean_reward, " -> ", last_episode_mean_reward)
+                model.save('models/' + envname + '_deepq.h5py')
+                last_checkpoint_mean_reward = last_episode_mean_reward
+
+            if is_done:
+                obs = env.reset()
+                last_episode_mean_reward = episode_total_reward / episode_timesteps
+                episode_total_reward = 0
+                episode_timesteps = 0
+                num_episodes += 1
+
+            if episode_render_freq != None and num_episodes % episode_render_freq == 0:
+                env.render()
 
 def main():
-    # Create a breakout environment
-    env = gym.make('Breakout-ram-v0')
-
-    learn(env)
-
+    # with tf.Session() as sess:
+    #     # sess.run(tf.global_variables_initializer())
+    #     # sess.run(tf.local_variables_initializer())
+    #     # learn('Breakout-ram-v0')
+    learn('CartPole-v0')
 
 # env.unwrapped.get_action_meanings()
 
